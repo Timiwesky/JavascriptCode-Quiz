58ba3fdcc07af95ccdaac61516edc85576c9df0a
timiwesco <timiwesco@yahoo.com>
2023-12-08 15:26:01 +0000

    Getting DOM elements

---
+++ assets/js/logic.js
@@ -0,0 +1,13 @@
+//  getting DOM elements
+
+const time = document.querySelector("#time");
+const startScreen = document.querySelector("#start-screen");
+const startButton = document.querySelector("#start");
+const questions = document.querySelector("#questions");
+const questionTitle = document.querySelector("#question-title");
+const choices = document.querySelector("#choices");
+const endScreen = document.querySelector("#end-screen");
+const finalScore = document.querySelector("#final-score");
+const submitBtn = document.querySelector("#submit");
+const feedback = document.querySelector("#feedback");
+const initials = document.querySelector("#initials");
