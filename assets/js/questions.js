519a15c63765f8f71e87bff96c033f80d0d2467e
timiwesco <timiwesco@yahoo.com>
2023-12-08 15:19:55 +0000

    An object of questions added

---
+++ assets/js/questions.js
@@ -0,0 +1,45 @@
+// An object with set of questions
+const qtnsObject = [
+    {
+        id: 1,
+        question: "What is the purpose of the 'addEventListener' method in JavaScript?",
+        A: "To remove an event listener",
+        B: "To add a new property",
+        C: "To handle events",
+        answer: "C"
+      },
+    {
+      id: 2,
+      question: "Which keyword is used to declare a variable in JavaScript?",
+      A: "variable",
+      B: "var",
+      C: "v",
+      answer: "B"
+    },
+    {
+        id: 3,
+        question: "What is the purpose of the 'indexOf' method in JavaScript?",
+        A: "To find the index of the first occurrence of a specified value in a string",
+        B: "To find the index of the last occurrence of a specified value in an array",
+        C: "To find the index of a specified value in an array",
+        answer: "C"
+      },
+    {
+      id: 4,
+      question: "How do you write a comment in JavaScript?",
+      A: "// This is a comment",
+      B: "/* This is a comment */",
+      C: "# This is a comment",
+      answer: "A"
+    },
+    {
+        id: 10,
+        question: "What is the purpose of the 'JSON.stringify()' method in JavaScript?",
+        A: "To parse a JSON string",
+        B: "To convert an object to a JSON string",
+        C: "To validate JSON syntax",
+        answer: "B"
+      },
+
+  ];
+  
